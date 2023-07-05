29cf373d793784cb3efff9cb144b6de688c3da2a
Regine <reginenjock@gmail.com>
2023-07-05 15:31:23 +0200

    done admin

--- Drink_Buddy/drink_service/admin.py
+++ Drink_Buddy/drink_service/admin.py
@@ -4,11 +4,14 @@ from .models import Drink, Drinks
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 7fe455edb026d0ce3910409b7c00da0ec03a61ba
 =======
 >>>>>>> 7fe455edb026d0ce3910409b7c00da0ec03a61ba
+=======
+>>>>>>> 4b2aa7825fe147d004283b971164cff550b25d9a
 =======
 >>>>>>> 4b2aa7825fe147d004283b971164cff550b25d9a
 =======
