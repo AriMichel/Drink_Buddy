d34ec33ab04f347c1fadb868375cbe3ad5bae840
Regine <reginenjock@gmail.com>
2023-07-05 15:49:30 +0200

    corrected admin.py

--- Drink_Buddy/drink_service/admin.py
+++ Drink_Buddy/drink_service/admin.py
@@ -1,24 +1,5 @@
 from django.contrib import admin
 from .models import Drink, Drinks
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 7fe455edb026d0ce3910409b7c00da0ec03a61ba
-=======
->>>>>>> 7fe455edb026d0ce3910409b7c00da0ec03a61ba
-=======
->>>>>>> 4b2aa7825fe147d004283b971164cff550b25d9a
-=======
->>>>>>> 4b2aa7825fe147d004283b971164cff550b25d9a
-=======
->>>>>>> 4b2aa7825fe147d004283b971164cff550b25d9a
-=======
->>>>>>> 4b2aa7825fe147d004283b971164cff550b25d9a
 # Register your models here.
 
 admin.site.register(Drink)
