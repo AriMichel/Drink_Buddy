9839d0123ad73e4a349bd4edae2b31c9c533692a
Regine <reginenjock@gmail.com>
2023-07-05 15:27:09 +0200

    done

--- Drink_Buddy/drink_service/admin.py
+++ Drink_Buddy/drink_service/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from .models import Drink, Drinks
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 7fe455edb026d0ce3910409b7c00da0ec03a61ba
 =======
 >>>>>>> 7fe455edb026d0ce3910409b7c00da0ec03a61ba
+=======
+>>>>>>> 4b2aa7825fe147d004283b971164cff550b25d9a
 # Register your models here.
 
 admin.site.register(Drink)
