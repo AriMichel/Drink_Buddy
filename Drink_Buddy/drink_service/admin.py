837e0e75fc637babf55d4199ac3dc6eff65dbcf8
Regine <reginenjock@gmail.com>
2023-07-05 15:18:42 +0200

    iniating admin.py

--- Drink_Buddy/drink_service/admin.py
+++ Drink_Buddy/drink_service/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Drink, Drinks
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7fe455edb026d0ce3910409b7c00da0ec03a61ba
 # Register your models here.
 
 admin.site.register(Drink)
