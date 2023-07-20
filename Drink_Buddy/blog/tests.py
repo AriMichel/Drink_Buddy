3ed75d2a199f6286ef01a618f68836ef7de0244f
Regine <reginenjock@gmail.com>
2023-07-20 02:38:28 +0200

    Modified Educational and removed about us from the home page and tested

--- Drink_Buddy/blog/tests.py
+++ Drink_Buddy/blog/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from django.utils import timezone
@@ -26,6 +26,48 @@ class PostModelTest(TestCase):
         expected_url = '/post/1/'.format(self.post.pk)
         self.assertEqual(self.post.get_absolute_url(), expected_url)
 
+class PostViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='This is a test post.',
+            author=self.user
+        )
+
+    def test_post_list_view(self):
+        request = self.factory.get(reverse('post'))
+        response = PostListView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post_home.html')
+
+    def test_post_detail_view(self):
+        request = self.factory.get(reverse('post-detail', kwargs={'pk': self.post.pk}))
+        response = PostDetailView.as_view()(request, pk=self.post.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post_detail.html')
+
+    def test_post_create_view(self):
+        request = self.factory.post(reverse('post-create'), data={'title': 'New Post', 'content': 'This is a new post.'})
+        request.user = self.user
+        response = PostCreateView.as_view()(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('post/2/', kwargs={'pk': 2}))
+
+    def test_post_update_view(self):
+        request = self.factory.post(reverse('post-update', kwargs={'pk': self.post.pk}), data={'title': 'Updated Post', 'content': 'This post has been updated.'})
+        request.user = self.user
+        response = PostUpdateView.as_view()(request, pk=self.post.pk)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('post-detail', kwargs={'pk': self.post.pk}))
+
+    def test_post_delete_view(self):
+        request = self.factory.post(reverse('post-delete', kwargs={'pk': self.post.pk}))
+        request.user = self.user
+        response = PostDeleteView.as_view()(request, pk=self.post.pk)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('post-list'))
 
 
-  
