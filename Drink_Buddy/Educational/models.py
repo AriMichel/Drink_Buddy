b6ef69f72cdac502f481fbc750a409c7761aafa0
Regine <reginenjock@gmail.com>
2023-08-09 15:52:44 +0200

    modified the models.py

--- Drink_Buddy/Educational/models.py
+++ Drink_Buddy/Educational/models.py
@@ -5,7 +5,7 @@ class Educational(models.Model):
     title= models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
-    url = models.URLField(blank=True)
+    
     
     def __str__(self):
         return self.title
